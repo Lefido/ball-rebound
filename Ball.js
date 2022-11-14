60c431cdfb96e05f6b8d8aa41af341dee3354f3b
Lefido <lefido@hotmail.com>
2022-11-14 15:40:34 +0100

    Two commit

--- Ball.js
+++ Ball.js
@@ -11,7 +11,7 @@ export default class Ball {
         ball.classList.add('ball')
       
         // ball.style.backgroundColor = `rgb(${100 + rand(155)},${100 + rand(155)},${100 + rand(155)})`
-        let numBall = 1 + rand(6);
+        let numBall = 1 + rand(8);
         // console.log(numBall)
         ball.style.background =  `url('assets/ball_${numBall}.png') center / cover`
         ball.style.width = taille + "px"
